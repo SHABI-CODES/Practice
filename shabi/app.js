d57c7eda614864e21e39745119b923e75f3e42f4
Muhammad Shahbaz <shahbaz94573@gmail.com>
2024-09-05 19:52:02 +0500

    Add files via upload

---
+++ shabi/app.js
@@ -0,0 +1,32 @@
+// 
+// alert("My name is Shahbaz")
+
+// var num_1=23;
+// var num_2=3;
+// var num3=num_1+23;
+// console.log(num_1+num_2+num3)
+
+// var fullname = Shahbaz;
+// document.write(fullname )
+// Console = "apna college";
+// console.log(Console);
+// var num1= 2 + 3 + (5*3);
+// document.write=(num1);
+// var result = 1 + 2;
+// result1 = 56;
+// // console.log (result + result1 + 1)
+// x = null;
+
+// isfollow = false;
+// // isfollow = true;
+// // fullname= "tony stark";
+// // fullname= 25;
+// // console.log(fullname)
+// var x = prompt("Where does the Pope live?");
+//   if (x === "Karachi") {
+//     document.write("Correct!");
+//   }
+//   if (x !== "Karachi") {
+//     document.write("Wrong answer");
+//   }
+
